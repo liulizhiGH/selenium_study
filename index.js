c32c6efeb32f433f3d9735e16e3cf2e39916ac21
liulizhiGH <liulizhiasd777@163.com>
2021-11-27 19:28:16 +0800

    init prj

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
+/*
+ * Author: LIULIZHI (liulizhiasd777@163.com)
+ * File Created: 2021-11-24 11:47:12 pm
+ * Last Modified: 2021-11-27 7:25:19 pm
+ * Description: 使用selenium浏览器自动化
+ */
+const fs = require("fs");
 const { Builder, By, Key, until } = require("selenium-webdriver");
 
 (async function auto() {
   const driver = new Builder().forBrowser("chrome").build();
   try {
     await driver.get("https://ant.design/docs/react/introduce-cn");
-
-    setInterval(() => {
-      console.log("执行栈");
-    }, 1000);
+    const base64 = await driver.takeScreenshot();
+    fs.writeFileSync("D:\\Desktop\\express_study\\public\\screenshot.png", base64, "base64");
   } catch (error) {
     console.log(err, "出错了");
     await driver.quit();
   } finally {
+    await driver.quit();
   }
 })();
